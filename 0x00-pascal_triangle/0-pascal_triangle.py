ee169f705baf793f1bcf6cf506e66d554f153f5b
Roro1839 <ronaldokipkirui90@gmail.com>
2024-01-17 18:05:18 +0300

    new

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,7 +1,20 @@
-#!/usr/bin/env python3
-from math import factorial
+#!/usr/bin/python3
+"""A Module (function) that creates Pascal's triangle"""
+
 
 def pascal_triangle(n):
+    """Creates a list of lists of integers representing
+    the Pascal's triangle of a given integer.
+    """
+    triangle = []
+    if type(n) is not int or n <= 0:
+        return triangle
     for i in range(n):
-        for j in range(i+1):
-            return(factorial(i)//(factorial(j)*factorial(i-j)))
+        line = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                line.append(1)
+            elif i > 0 and j > 0:
+                line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        triangle.append(line)
+    return triangle
