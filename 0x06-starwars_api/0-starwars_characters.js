095fb203000a5df1add467de798e96471b8335a2
Roro1839 <ronaldokipkirui90@gmail.com>
2024-01-19 23:15:04 +0300

    good

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,46 @@
 #!/usr/bin/node
-
 const request = require('request');
 
-const movieId = process.argv[2];
-const url = `https://swapi.dev/api/films/${movieId}/`;
-request(url, async function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    const characters = JSON.parse(body).characters;
-    for (const character of characters) {
-      const res = await new Promise((resolve, reject) => {
-        request(character, (error, res, html) => {
-          if (error) {
-            reject(error);
-          } else {
-            resolve(JSON.parse(html).name);
-          }
-        });
-      });
-      console.log(res);
-    }
+function fetch (url) {
+  return new Promise((resolve, reject) => {
+    request.get(url, (err, response, body) => {
+      if (err) {
+        reject(err);
+      } else if (response.statusCode === 200) {
+        resolve(body);
+      } else {
+        reject(new Error(`Request failed. Status code: ${response.statusCode}`));
+      }
+    });
+  });
+}
+
+function fetchCharactersRecursively (charData, index) {
+  if (index >= charData.length) {
+    return;
   }
-});
+
+  const characterUrl = charData[index];
+
+  fetch(characterUrl).then(body => {
+    const character = JSON.parse(body);
+    console.log(character.name);
+    fetchCharactersRecursively(charData, index + 1);
+  }).catch(err => {
+    console.error(err);
+    fetchCharactersRecursively(charData, index + 1);
+  });
+}
+
+function fetchMovieCharacters (movieID) {
+  const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieID}/`;
+
+  fetch(apiUrl).then(body => {
+    const movieData = JSON.parse(body);
+    const charData = movieData.characters;
+    fetchCharactersRecursively(charData, 0);
+  }).catch(err => console.error(err));
+}
+
+const movieID = process.argv[2];
+fetchMovieCharacters(movieID);
